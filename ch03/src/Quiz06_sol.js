443d9f6d6da7a5b4b1d5d9541c0e9d3aa22beb4c
hado0123 <hado_123@naver.com>
2024-08-27 21:09:38 +0900

    up

---
+++ ch03/src/Quiz06_sol.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+function Quiz06_sol() {
+    const [count, setCount] = useState(0)
+
+    return (  
+        <div>
+            <p>카운트: { count }</p>
+            <button onClick={() => setCount(count + 1)}>
+                1씩 증가
+            </button>
+        </div>
+    );
+}
+
+export default Quiz06_sol;
