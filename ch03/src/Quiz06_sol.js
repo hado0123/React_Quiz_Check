d68914a4fed247e57b94f3adf452b87300dfa36f
hado0123 <hado_123@naver.com>
2025-06-02 18:01:05 +0900

    up

--- ch03/src/Quiz06_sol.js
+++ ch03/src/Quiz06_sol.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react'
 
 function Quiz06_sol() {
+   // state는 set 함수를 이용해서 바꿀 수 있으므로 const로 선언해서 사용
    const [count, setCount] = useState(0)
 
-   // setCount(count++) => 사용 X => 리액트에서는 state값은 반드시 set 함수로 변화시킬 수 있으므로. 값을 직접 변화 시킬 수 X
-   // count++ 은 count = count + 1 이므로
    return (
       <div>
          <p>카운트: {count}</p>
          <button
             onClick={() => {
                setCount(count + 1)
-               //setCount(count++) // count = count + 1 -> 증감연산자는 count값 자체를 바꾸므로 사용할수 X(count는 const로 선언되어있음) -> count는 setCount함수를 이용해서 바꾸면 된다
+               // setCount(count++) count state는 const로 선언되서 직접 count값을 바꿀 수 X
             }}
          >
             1씩 증가
